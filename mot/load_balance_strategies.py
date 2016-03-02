04585fdad64c7e63a345f00f3d0a332fe981b6e3
Robbert Harms <robbert@xkls.nl>
2016-03-02 12:34:39 +0100

    Testing new memory buffer layout with the log likelihood

--- mot/load_balance_strategies.py
+++ mot/load_balance_strategies.py
@@ -113,7 +113,7 @@ class Worker(object):
 
 class LoadBalanceStrategy(object):
 
-    def __init__(self, run_in_batches=True, single_batch_length=2):
+    def __init__(self, run_in_batches=True, single_batch_length=4000):
         """ The base load balancer.
 
         Every load balancer has the option to run the calculations in batches. The advantage of batches is that it is
