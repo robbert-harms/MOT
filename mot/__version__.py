8b81c36dc8019a827f2fb37f8cc5c253c81daed0
Robbert Harms <robbert.harms@maastrichtuniversity.nl>
2017-04-09 10:11:24 +0200

    Small update in the dependent parameter computation. This should be more friendly to low memory devices

--- mot/__version__.py
+++ mot/__version__.py
@@ -1,4 +1,4 @@
-VERSION = '0.2.37'
+VERSION = '0.2.38'
 
 _items = VERSION.split('-')
 VERSION_NUMBER_PARTS = tuple(int(i) for i in _items[0].split('.'))
--- mot/cl_routines/mapping/calc_dependent_params.py
+++ mot/cl_routines/mapping/calc_dependent_params.py
@@ -117,7 +117,7 @@ class _CDPWorker(Worker):
         parameter_write_out = ''
         for i, p in enumerate(dependent_parameter_names):
             parameter_write_out += 'results[gid * ' + str(len(dependent_parameter_names)) + \
-                                   ' + ' + str(i) + '] = ' + p + ";\n"
+                                   ' + ' + str(i) + '] = _get_' + p + '(&x, data);\n'
 
         kernel_param_names = ['global mot_float_type* params', 'global mot_float_type* results']
         kernel_param_names.extend(self._model.get_kernel_param_names(self._cl_environment.device))
@@ -125,6 +125,18 @@ class _CDPWorker(Worker):
         kernel_source = ''
         kernel_source += get_float_type_def(self._double_precision)
         kernel_source += self._model.get_kernel_data_struct(self._cl_environment.device)
+
+        for dependent_parameter_name in dependent_parameter_names:
+            kernel_source += '''
+            mot_float_type _get_''' + dependent_parameter_name + '''(
+                    mot_float_type* x,
+                    ''' + self._model.get_kernel_data_struct_type() + '''* data){{
+
+                ''' + self._parameters_listing + '''
+                return ''' + dependent_parameter_name + ''';
+            }}
+            '''
+
         kernel_source += '''
             __kernel void transform(
                 ''' + ",\n".join(kernel_param_names) + '''
@@ -140,7 +152,7 @@ class _CDPWorker(Worker):
                     for(i = 0; i < ''' + str(self._nmr_estimated_params) + '''; i++){
                         x[i] = params[gid * ''' + str(self._nmr_estimated_params) + ''' + i];
                     }
-                    ''' + self._parameters_listing + '''
+
                     ''' + parameter_write_out + '''
             }
         '''
