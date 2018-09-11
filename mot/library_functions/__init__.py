bb28e6df6a1acf035c4e8647f7697ca3d0b93907
Robbert Harms <robbert.harms@maastrichtuniversity.nl>
2018-09-11 10:08:08 +0200

    Adds CL function to calculate the even Legendre terms of the Legendre polynomial.

--- mot/library_functions/__init__.py
+++ mot/library_functions/__init__.py
@@ -42,7 +42,7 @@ class FirstLegendreTerm(SimpleCLLibrary):
         * x: The argument of the Legendre polynomial Pn.
         * n: The degree of the Legendre polynomial Pn.
 
-        The return value is Pn(x) if n is a nonnegative integer.  If n is negative, 0 is returned.
+        The return value is Pn(x) if n is a non negative integer.  If n is negative, 0 is returned.
         """
         super().__init__('''
             double firstLegendreTerm(double x, int n){
@@ -75,6 +75,69 @@ class FirstLegendreTerm(SimpleCLLibrary):
                 }
         
                 return Pn;
+            }
+        ''')
+
+
+class EvenLegendreTerms(SimpleCLLibrary):
+    def __init__(self):
+        """Compute a range of even legendre terms for the given value x and the polynomial degree n.
+
+        The Legendre polynomials, Pn(x), are orthogonal on the interval [-1,1] with weight function w(x) = 1
+        for -1 <= x <= 1 and 0 elsewhere.  They are normalized so that Pn(1) = 1.  The inner products are:
+
+        .. code-block:: c
+
+            <Pn,Pm> = 0        if n != m,
+            <Pn,Pn> = 2/(2n+1) if n >= 0.
+
+
+        This routine calculates Pn(x) for all n in [0, 2, 4, ..., n] using the following recursion:
+
+        .. code-block:: c
+
+            (k+1) P[k+1](x) = (2k+1)x P[k](x) - k P[k-1](x), k = 1,...,n-1
+            P[0](x) = 1, P[1](x) = x.
+
+        That is, this function will fill the given array legendre_terms with the values:
+            [0] = firstLegendreTerm(x, 0)
+            [1] = firstLegendreTerm(x, 2)
+            [2] = firstLegendreTerm(x, 4)
+            [3] = firstLegendreTerm(x, 8)
+
+        The function arguments are:
+
+            x: The argument of the Legendre polynomial Pn.
+            n: The maximum degree of the Legendre polynomial Pn.
+            legendre_terms: an matrix of length n/2 for storing the even legendre terms
+        """
+        super().__init__('''
+            void EvenLegendreTerms(double x, int n, double* legendre_terms){
+                if(fabs(x) == 1.0){
+                    // the legendre terms collapse to 1 for even terms if fabs(x) == 1.0
+                    for(int i = 0; i < n; i++){
+                        legendre_terms[i] = 1.0;
+                    }
+                    return;
+                }
+    
+                legendre_terms[0] = 1.0;
+    
+                mot_float_type P0 = 1.0;
+                mot_float_type P1 = x;
+                mot_float_type Pn;
+    
+                for(int k = 1; k < n; k++){
+                    Pn = ((2 * (2*k-1) + 1) * x * P1 - ((2*k-1) * P0)) / ((2*k-1) + 1);
+                    P0 = P1;
+                    P1 = Pn;
+    
+                    legendre_terms[k] = Pn;
+    
+                    Pn = ((2 * ((2*k-1)+1) + 1) * x * P1 - (((2*k-1)+1) * P0)) / (((2*k-1)+1) + 1);
+                    P0 = P1;
+                    P1 = Pn;
+                }
             }
         ''')
 
