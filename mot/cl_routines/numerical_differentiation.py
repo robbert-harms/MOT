12573407acd3398bf1d66a2e4833218f81747002
Robbert Harms <robbert.harms@maastrichtuniversity.nl>
2019-12-09 15:35:23 +0100

    Small documentation update

--- mot/cl_routines/numerical_differentiation.py
+++ mot/cl_routines/numerical_differentiation.py
@@ -58,10 +58,10 @@ def estimate_hessian(objective_func, parameters,
 
         parameters (ndarray): The parameters at which to evaluate the gradient. A (d, p) matrix with d problems,
             and p parameters
-        lower_bounds (list or None): a list of length (p,) for p parameters with the lower bounds.
+        lower_bounds (tuple or list or None): a list of length (p,) for p parameters with the lower bounds.
             Each element of the list can be a scalar or a vector (of the same length as the number
             of problem instances). To disable bounds for this parameter use -np.inf.
-        upper_bounds (list or None): a list of length (p,) for p parameters with the upper bounds.
+        upper_bounds (tuple or list or None): a list of length (p,) for p parameters with the upper bounds.
             Each element of the list can be a scalar or a vector (of the same length as the number
             of problem instances). To disable bounds for this parameter use np.inf.
         step_ratio (float): the ratio at which the steps diminish.
@@ -120,7 +120,7 @@ def estimate_hessian(objective_func, parameters,
             for(int i = 0; i < ''' + str(nmr_params) + '''; i++){
                 for(int j = i; j < ''' + str(nmr_params) + '''; j++){
                     _numdiff_hessian_element(
-                        data, x_tmp, f_x_input, i, j, initial_step, 
+                        data, x_tmp, f_x_input, i, j, initial_step,
                         derivatives + coord_ind, errors + coord_ind, scratch);
 
                     coord_ind++;
@@ -139,45 +139,45 @@ def _get_numdiff_hessian_element_func(objective_func, nmr_steps, step_ratio):
     """Return a function to compute one element of the Hessian matrix."""
     return SimpleCLFunction.from_string('''
         /**
-         * Compute the Hessian using (possibly) multiple steps with various interpolations. 
-         */ 
+         * Compute the Hessian using (possibly) multiple steps with various interpolations.
+         */
         void _numdiff_hessian_element(
                 void* data, local mot_float_type* x_tmp, mot_float_type f_x_input,
-                uint px, uint py, global float* initial_step, global double* derivative, 
+                uint px, uint py, global float* initial_step, global double* derivative,
                 global double* error, local double* scratch){
 
             const uint nmr_steps = ''' + str(nmr_steps) + ''';
             uint nmr_steps_remaining = nmr_steps;
 
             local double* scratch_ind = scratch;
-            local double* steps = scratch_ind;      scratch_ind += nmr_steps; 
+            local double* steps = scratch_ind;      scratch_ind += nmr_steps;
             local double* errors = scratch_ind;     scratch_ind += nmr_steps - 1;
             local double* steps_tmp = scratch_ind;  scratch_ind += nmr_steps;
-            
+
             if(get_local_id(0) == 0){
                 for(int i = 0; i < nmr_steps - 1; i++){
                     errors[i] = 0;
                 }
             }
             barrier(CLK_LOCAL_MEM_FENCE);
-            
+
             _numdiff_hessian_steps(data, x_tmp, f_x_input, px, py, steps, initial_step);
 
             if(nmr_steps_remaining > 1){
-                nmr_steps_remaining = _numdiff_hessian_richardson_extrapolation(steps); 
+                nmr_steps_remaining = _numdiff_hessian_richardson_extrapolation(steps);
                 barrier(CLK_LOCAL_MEM_FENCE);
             }
-            
+
             if(nmr_steps_remaining >= 3){
                 nmr_steps_remaining = _numdiff_wynn_extrapolation(steps, errors, nmr_steps_remaining);
-                barrier(CLK_LOCAL_MEM_FENCE);                
+                barrier(CLK_LOCAL_MEM_FENCE);
             }
-            
+
             if(nmr_steps_remaining > 1){
                 _numdiff_find_best_step(steps, errors, steps_tmp, nmr_steps_remaining);
                 barrier(CLK_LOCAL_MEM_FENCE);
             }
-            
+
             if(get_local_id(0) == 0){
                 *derivative = steps[0];
                 *error = errors[0];
@@ -196,23 +196,23 @@ def _get_numdiff_hessian_steps_func(objective_func, nmr_steps, step_ratio):
     return SimpleCLFunction.from_string('''
         /**
          * Compute one element of the Hessian for a number of steps.
-         * 
+         *
          * This uses the initial steps in the data structure, indexed by the parameters to change (px, py).
          *
          * Args:
          *  data: the data container
          *  x_tmp: the array with the input parameters, needs to be writable, although it will return
          *         the same values.
-         *  f_x_input: the objective function value at the original set of parameters  
+         *  f_x_input: the objective function value at the original set of parameters
          *  px: the index of the first parameter to perturbate
          *  py: the index of the second parameter to perturbate
          *  steps: storage location for the output steps
          *  initial_step: the initial steps, array of same length as x_temp
          */
-        void _numdiff_hessian_steps(void* data, local mot_float_type* x_tmp, 
+        void _numdiff_hessian_steps(void* data, local mot_float_type* x_tmp,
                                     mot_float_type f_x_input,
-                                    uint px, uint py, 
-                                    local double* steps, 
+                                    uint px, uint py,
+                                    local double* steps,
                                     global float* initial_step){
 
             double step_x;
@@ -228,7 +228,7 @@ def _get_numdiff_hessian_steps_func(objective_func, nmr_steps, step_ratio):
                           _numdiff_hessian_eval_step_mono(data, x_tmp, px, 2 * step_x)
                         + _numdiff_hessian_eval_step_mono(data, x_tmp, px, -2 * step_x)
                         - 2 * f_x_input
-                    ) / (4 * step_x * step_x);    
+                    ) / (4 * step_x * step_x);
 
                     if(is_first_workitem){
                         steps[step_ind] = tmp;
@@ -249,7 +249,7 @@ def _get_numdiff_hessian_steps_func(objective_func, nmr_steps, step_ratio):
 
                     if(is_first_workitem){
                         steps[step_ind] = tmp;
-                    }                       
+                    }
                 }
             }
         }
@@ -268,7 +268,7 @@ def _get_numdiff_hessian_steps_func(objective_func, nmr_steps, step_ratio):
          *  the function evaluated at the parameters plus their perturbation.
          */
         double _numdiff_hessian_eval_step_mono(
-                void* data, local mot_float_type* x_tmp, 
+                void* data, local mot_float_type* x_tmp,
                 uint perturb_dim_0, mot_float_type perturb_0){
 
             mot_float_type old_0;
@@ -307,7 +307,7 @@ def _get_numdiff_hessian_steps_func(objective_func, nmr_steps, step_ratio):
          *  the function evaluated at the parameters plus their perturbation.
          */
         double _numdiff_hessian_eval_step_bi(
-                void* data, local mot_float_type* x_tmp, 
+                void* data, local mot_float_type* x_tmp,
                 uint perturb_dim_0, mot_float_type perturb_0,
                 uint perturb_dim_1, mot_float_type perturb_1){
 
@@ -342,34 +342,34 @@ def _get_numdiff_hessian_richardson_extrapolation_func(nmr_steps, step_ratio):
     return SimpleCLFunction.from_string('''
         /**
          * Apply the Richardson extrapolation to the derivatives computed with multiple steps.
-         * 
-         * Having for every problem instance and every Hessian element multiple derivatives computed 
+         *
+         * Having for every problem instance and every Hessian element multiple derivatives computed
          * with decreasing steps, we can now apply Richardson extrapolation to reduce the error term from O(h^2) to
          * O(h^4) or O(h^6) depending on how many steps we have calculated.
          *
          * This method only considers extrapolation up to the sixth error order. For a set of two derivatives we compute
-         * a single fourth order approximation, for three derivatives or more, we compute ``n-2`` sixth order 
-         * approximations. 
-         * 
+         * a single fourth order approximation, for three derivatives or more, we compute ``n-2`` sixth order
+         * approximations.
+         *
          * Args:
-         *  steps: on input, the steps we are convoluting with Richardson extrapolation. On output, 
+         *  steps: on input, the steps we are convoluting with Richardson extrapolation. On output,
          *         the convoluted steps, these are less than the input, see the return value.
-         * 
+         *
          * Returns:
          *  nmr_steps: the number of steps remaining after convolution
          */
         uint _numdiff_hessian_richardson_extrapolation(local double* steps){
-            
+
             const uint nmr_steps = ''' + str(nmr_steps) + ''';
             uint nmr_steps_remaining = nmr_steps;
-            
+
             if(get_local_id(0) == 0){
                 // 4th order approximations
                 for(uint i = 0; i < nmr_steps - 1; i++){
                     steps[i] = richardson_extrapolate(steps[i], steps[i + 1], ''' + str(step_ratio) + ''', 2);
                 }
                 nmr_steps_remaining--;
-                
+
                 // 6th order approximations
                 for(uint i = 0; i < nmr_steps - 2; i++){
                     steps[i] = richardson_extrapolate(steps[i], steps[i + 1], ''' + str(step_ratio) + ''', 4);
@@ -387,9 +387,9 @@ def _get_numdiff_find_best_step_func():
         /**
          * Find the step with the lowest error.
          *
-         * This will first apply a median filter on the steps to locate derivatives which deviate strongly 
-         * from the distribution of the steps. These deviations are added as extra error term to the error term from 
-         * the extrapolations. Afterwards it chooses the derivative with the lowest error. 
+         * This will first apply a median filter on the steps to locate derivatives which deviate strongly
+         * from the distribution of the steps. These deviations are added as extra error term to the error term from
+         * the extrapolations. Afterwards it chooses the derivative with the lowest error.
          *
          * Args:
          *  steps: on input, array with the input steps. On output, a single step with the best derivative
@@ -408,49 +408,49 @@ def _get_numdiff_find_best_step_func():
                         all_nan = false;
                     }
                 }
-    
+
                 if(all_nan){
                     steps[0] = NAN;
                     errors[0] = NAN;
-    
+
                     return;
                 }
-    
+
                 _get_median_outlier_error(steps, scratch, nmr_steps);
-                
+
                 double lowest_error = INFINITY;
-                int ind_lowest_error = 0; 
+                int ind_lowest_error = 0;
                 for(int i = 0; i < nmr_steps; i++){
                     if(errors[i] + scratch[i] < lowest_error){
                         lowest_error = errors[i] + scratch[i];
                         ind_lowest_error = i;
-                    } 
+                    }
                 }
-    
+
                 steps[0] = steps[ind_lowest_error];
-                errors[0] = errors[ind_lowest_error] + scratch[ind_lowest_error];    
+                errors[0] = errors[ind_lowest_error] + scratch[ind_lowest_error];
             }
         }
     ''', dependencies=[SimpleCLFunction.from_string('''
         /**
-         * This function tries to detect outliers by means of an median filter. 
-         * If a value is a factor of 10 to 1 in either direction away from the median, we weight it 
+         * This function tries to detect outliers by means of an median filter.
+         * If a value is a factor of 10 to 1 in either direction away from the median, we weight it
          * with an additional error.
          *
          * This uses a hardcoded trim factor of 10 to 1 as relative distance.
-         * 
+         *
          * Args:
-         *  steps: 
-         * 
+         *  steps:
+         *
          */
         void _get_median_outlier_error(local double* steps, local double* median_errors, uint nmr_steps){
-            float trim_factor = 10;   
+            float trim_factor = 10;
             int i;
 
             local double* sorted_values = median_errors;
             for(int i = 0; i < nmr_steps; i++){
                 sorted_values[i] = steps[i];
-            }            
+            }
             _numdiff_sort_values(sorted_values, nmr_steps);
 
             double p25 = _numdiff_get_percentile(sorted_values, nmr_steps, 0.25);
@@ -467,15 +467,15 @@ def _get_numdiff_find_best_step_func():
                               ((steps[i] < p25 - 1.5 * iqr) + (p75 + 1.5 * iqr < steps[i])));
 
                 median_errors[i] = is_outlier * fabs(steps[i] - p50);
-            }            
+            }
         }
 
     ''', dependencies=[SimpleCLFunction.from_string('''
-            /** 
+            /**
              * Uses bubblesort to sort the given values in ascending order.
-             * 
+             *
              */
-            void _numdiff_sort_values(local double* values, int n){  
+            void _numdiff_sort_values(local double* values, int n){
                 int i, j;
                 double tmp;
 
@@ -484,7 +484,7 @@ def _get_numdiff_find_best_step_func():
                         if(values[j] > values[j + 1]){
                             tmp = values[j];
                             values[j] = values[j + 1];
-                            values[j + 1] = tmp;    
+                            values[j + 1] = tmp;
                         }
                     }
                 }
@@ -492,15 +492,15 @@ def _get_numdiff_find_best_step_func():
         '''), SimpleCLFunction.from_string('''
             /**
              * Find the distribution value for the requested percentile.
-             * 
-             * This uses linear interpolation with C == 1, as defined in 
+             *
+             * This uses linear interpolation with C == 1, as defined in
              * https://en.wikipedia.org/wiki/Percentile#The_nearest-rank_method.
-             * 
+             *
              * Args:
              *  sorted_values: array of sorted values
              *  nmr_values: number of values
              *  percentile: the requested percentile, between 0 and 1.
-             * 
+             *
              * Returns:
              *  the value for the percentile
              */
@@ -508,7 +508,7 @@ def _get_numdiff_find_best_step_func():
                 double rank = percentile * (nmr_values - 1);
                 int rank_int = (int) rank;
                 double fraction = rank - rank_int;
-                return sorted_values[rank_int] + fraction * (sorted_values[rank_int + 1] - sorted_values[rank_int]);    
+                return sorted_values[rank_int] + fraction * (sorted_values[rank_int + 1] - sorted_values[rank_int]);
             }
         ''')])])
 
@@ -530,28 +530,28 @@ def _get_numdiff_wynn_extrapolation_func():
          *           "Acceleration de la convergence en analyse numerique",
          *           "Lecture Notes in Math.", vol. 584,
          *           Springer-Verlag, New York, 1977.
-         * 
+         *
          *   - [3] E. J. Weniger (1989)
          *           "Nonlinear sequence transformations for the acceleration of
          *           convergence and the summation of divergent series"
          *           Computer Physics Reports Vol. 10, 189 - 371
          *           http://arxiv.org/abs/math/0306302v1
-         * 
+         *
          * Args:
          *  steps: on input, array with the input steps. On output, the extrapolated steps
          *  errors: the errors of the extrapolated steps
          *  nmr_steps: the number of steps in the input array
-         * 
+         *
          * Returns:
-         *  the number of steps in the output 
+         *  the number of steps in the output
          */
         uint _numdiff_wynn_extrapolation(local double* steps, local double* errors, uint nmr_steps){
             if(get_local_id(0) == 0){
-                double v0, v1, v2; 
+                double v0, v1, v2;
 
-                double delta0, delta1; 
-                double err0, err1; 
-                double tol0, tol1; 
+                double delta0, delta1;
+                double err0, err1;
+                double tol0, tol1;
 
                 double ss;
                 bool converged;
@@ -635,14 +635,14 @@ class richardson_extrapolation(SimpleCLLibrary):
             /**
              * Apply a Richardson extrapolation on the given input.
              *
-             * See https://en.wikipedia.org/wiki/Richardson_extrapolation. 
+             * See https://en.wikipedia.org/wiki/Richardson_extrapolation.
              *
              * Args:
              *  v1: the primary value at a step size of h
-             *  v2: the secondary value, computed at a step size of h/step_ratio 
+             *  v2: the secondary value, computed at a step size of h/step_ratio
              *  step_ratio: the ratio at which the steps diminish
              *  approximation_order: the order of the current approximations
-             * 
+             *
              * Returns:
              *   the Richardson extrapolation
              */
